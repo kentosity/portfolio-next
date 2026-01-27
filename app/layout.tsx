b303d2f06cec68bfc8efa30068a2a790166ede94
kentosity <kento.iizuka@generosity.co.jp>
2026-01-28 07:46:42 +0900

    add: clarity

--- app/layout.tsx
+++ app/layout.tsx
@@ -73,6 +73,19 @@ export default function RootLayout({
             `,
           }}
         />
+        <Script
+          id="clarity-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                  c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                  t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                  y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "v87uz3er5b");
+            `,
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[var(--background)] text-[var(--foreground)]`}
